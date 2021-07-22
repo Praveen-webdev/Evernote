24ab43cd3373671d2e41c06fdc5e42906fc25a96
github_Praveen-webdev <praveen1598810@gmail.com>
2021-07-22 13:42:13 +0530

    updated versions

---
+++ build/precache-manifest.6abd7120d6d8cff807bbe82cdde03658.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db057b0ca1f134a5d8d333251ea5e358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "debde6ba1ea823dee8d8",
+    "url": "/static/js/2.d3f45100.chunk.js"
+  },
+  {
+    "revision": "4e095000e7132b795a7f",
+    "url": "/static/js/main.658c0285.chunk.js"
+  },
+  {
+    "revision": "d1b10eb14fb98550d014",
+    "url": "/static/js/runtime-main.408ce3c7.js"
+  }
+]);
